0ddf22873a5dada3ce5936ed654712f692ec1287
Sivabalan Thirunavukkarasu <siva886@gmail.com>
2013-01-07 20:46:30 +0530

    v0.0.3 - Url check, selected edit tab on click

--- code-editor.js
+++ code-editor.js
@@ -13,41 +13,41 @@ var codeExtensionsDict = {
 	}
 }
 
-function setupCodeEditor(codeType) {
-	var codeArea = $('pre.'+codeType+'.source-'+codeType);
-	var codeContent = codeArea.text();
-	codeArea.replaceWith('<div id="editor">'+codeContent+'</div>');	 		
-	$('#editor').css('position','relative');
-	$('#editor').css('width','100%');
-	$('#editor').css('height','500px');
-
-	var editor = ace.edit("editor");
-	editor.setTheme("ace/theme/monokai");
-	editor.getSession().setMode(codeExtensionsDict[pageType]['aceMode']);
-}
-
 var pageType = wgPageName.split('.').pop();
+var codeEditMode = false;
 
 if(pageType in codeExtensionsDict)
 {
 	var editCodeLink = $('a[accesskey="e"]');
-	var origEditUrl = editCodeLink.attr('href');
-	var newUrl = document.URL+codeExtensionsDict[pageType]['urlStub'];
-
-	editCodeLink.attr('href',newUrl);
+	var editUrl = document.URL;
+			
+	editCodeLink.attr('href','javascript:void(0)');
 	editCodeLink.attr('title','Click to edit your '+pageType+' code');
 	editCodeLink.css('pointer-events','none');
 	editCodeLink.css('color','grey');
 
 	editCodeLink.click(function(){
-			setupCodeEditor(codeExtensionsDict[pageType]['fullName']);
+		codeEditMode = true;
+		$('#p-views li[id^="ca-"]').removeClass("selected");
+		$('#p-views li[id="ca-edit"]').addClass("selected");
+		
+		var codeArea = $('pre.'+codeExtensionsDict[pageType]['fullName']+'.source-'+codeExtensionsDict[pageType]['fullName']);
+		var codeContent = codeArea.text();
+		codeArea.replaceWith('<div id="editor">'+codeContent+'</div>');	 		
+		$('#editor').css('position','relative');
+		$('#editor').css('width','100%');
+		$('#editor').css('height','500px');
+
+		var editor = ace.edit("editor");
+		editor.setTheme("ace/theme/monokai");
+		editor.getSession().setMode(codeExtensionsDict[pageType]['aceMode']);
 	});
 	
 	$.getScript("http://d1n0x3qji82z53.cloudfront.net/src-min-noconflict/ace.js", function(data, textStatus, jqxhr) {
 			editCodeLink.css('pointer-events','auto');
 			editCodeLink.css('color','');	
 		});
-			
+	
 }
 
 	
