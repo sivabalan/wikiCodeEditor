1a14d067fd4b2d66dbc6f48d842118cb0aa55ee7
Sibi <psibi2000@gmail.com>
2013-01-14 02:36:08 +0530

    Includes lot of Modification.
    1) jsHint Handling removed since ACE Editor automatically handles
    them.
    2) UI Changed: Two Buttons now. Solves Issue #17.
    3) The Save Dialog is quite responsive now.

--- code-editor.js
+++ code-editor.js
@@ -1,6 +1,4 @@
-/* page: User:{{USERNAME}}/wikiCodeEditor/code-editor.js */
-$(function() {
-    
+page: User:Psibi_tux/wikiCodeEditor/code-editor.js
 var codeExtensionsDict = {
 	'js' : {
 		'urlStub' : "#editJs",
@@ -48,115 +46,72 @@ var editor;
 
 if(pageType in codeExtensionsDict)
 {
-	var editCodeLink = $('a[accesskey="e"]');
-	var editUrl = document.URL;
-			
-	editCodeLink.attr('href','javascript:void(0)');
-	editCodeLink.attr('title','Click to edit your '+pageType+' code');
-	editCodeLink.css('pointer-events','none');
-	editCodeLink.css('color','grey');
-
-	editCodeLink.click(function(){
-		codeEditMode = true;
-		$('#p-views li[id^="ca-"]').removeClass("selected");
-		$('#p-views li[id="ca-edit"]').addClass("selected");
-		
-		var codeArea = $('pre.'+codeExtensionsDict[pageType]['fullName']+'.source-'+codeExtensionsDict[pageType]['fullName']);
-		var codeContent = escape(codeArea.text());
-		codeArea.replaceWith('<div id="toolBar"></div><div id="editor"></div>');
-		$('#editor').css('position','relative');
-		$('#editor').css('width','100%');
-		$('#editor').css('height','500px');
-
-		editor = ace.edit("editor");
-		editor.setValue(unescape(codeContent));
-		editor.setTheme("ace/theme/monokai");
-		editor.getSession().setMode(codeExtensionsDict[pageType]['aceMode']);
-
-		mw.loader.using('mediawiki.api.edit', function() {
-			$('#toolBar').html('<div class="codeEditOptions" style="position: relative;background-color: #F0F0F0;height: 30px;padding: 10px 10px 5px 10px;"><label style="float:left; line-height:24px">Editor theme : </label><select id="themeSelect" style="float: left; margin-left: 5px;margin-top: 3px;"></select><div id="jshint" style="margin-left: 5px;float: left;display: none"><input type="button" value="jsHint"></div><div class="editButtons" style="float: right;"><input id="wpCodeSave" type="submit" tabindex="5" value="Save" accesskey="s" title="Save your changes [alt-s]"><input id="wpCodeSaveDone" type="submit" tabindex="6" value="Save and close" accesskey="p" title="Save you changes and refresh the page [alt-p]"><span class="cancelLink"><a href="javascript:void(0)" title="Cancel edit" id="mw-editform-cancel">Cancel</a></span></div><input class="mw-summary" id="wpSummary" maxlength="255" tabindex="1" size="60" spellcheck="true" title="Enter a short summary [alt-b]" accesskey="b" name="wpSummary" placeholder="Enter a brief summary of your changes" style="margin-top: 3px;margin-right: 5px;width: 50%;float: right;height: 17px;"><!-- editButtons --></div>');
-
-			for(var key in themeDict)
-			{
-				$('#themeSelect').append('<option value="ace/theme/'+themeDict[key]+'">'+key+'</option>');
-			}
-
-		    $('#themeSelect').change(function(){
-			var themeString = $('#themeSelect').val();
-			editor.setTheme(themeString.toString());
-		    });
-                    if (wgPageName.match(/js$/g))
-                        {
-                            $('#jshint').css('display','');
-                        }
-                    $('#jshint').click(function() {
-                        var result = JSHINT(editor.getValue());
-                        if (result === false)
-                            {
-                                var jerrors = $(JSHINT.errors);
-                                var NewDialog = $('<div id="MenuDialog" title="jsHint"></div>');
-                                jerrors.each( function (index) { 
-                                    var error = "Line "+ this.line + ", " + this.evidence + ",Reason: " + this.reason;
-                                    NewDialog.append("<p>"+error+"</p>");
-                                });
-                                NewDialog.dialog()
-                            }
-                        else
-                            {
-                                var NewDialog = $('<div id="MenuDialog" title="jsHint"><p>Good code. No errors!!</p></div>');
-                                NewDialog.dialog();
-                            }
-                    });
-
-			function saveCode(action) {
-				var edit = new mw.Api();
-				var params = {
-					action: 'edit',
-					title: mw.config.get('wgPageName'),
-					text: editor.getSession().getValue(),
-					summary: $('#wpSummmary').val()
-				};
-				edit.postWithEditToken(params).done(function(data){
-					if(('edit' in data) && (data.edit.result == 'Success')){
-						mw.util.jsMessage('Your code is saved');
-					}
-					else if('warnings' in data){
-						mw.util.jsMessage('Code save error');
-					}
-					if(action == 'reload')
-					{
-						location.reload(true);
-					}
-				});
-			}
-
-			$('#wpCodeSave').click(function(){
-				saveCode('save');	
-			});
-
-			$('#wpCodeSaveDone').click(function(){
-				saveCode('reload');
-			});
-
-			$('span.cancelLink').click(function(){
-				location.reload(true);
-			});
-		});
-
-	});
+    var editCodeLink = $('a[accesskey="e"]');
+    var editUrl = document.URL;
+    
+    editCodeLink.attr('href','javascript:void(0)');
+    editCodeLink.attr('title','Click to edit your '+pageType+' code');
+    editCodeLink.css('pointer-events','none');
+    editCodeLink.css('color','grey');
+    
+    editCodeLink.click(function(){
+	codeEditMode = true;
+	$('#p-views li[id^="ca-"]').removeClass("selected");
+	$('#p-views li[id="ca-edit"]').addClass("selected");
 	
-	$.getScript("//d1n0x3qji82z53.cloudfront.net/src-min-noconflict/ace.js", function(data, textStatus, jqxhr) {
-			editCodeLink.css('pointer-events','auto');
-			editCodeLink.css('color','');	
-		});
-	
-    if (wgPageName.match(/js$/g))
-    {
+	var codeArea = $('pre.'+codeExtensionsDict[pageType]['fullName']+'.source-'+codeExtensionsDict[pageType]['fullName']);
+	var codeContent = escape(codeArea.text());
+	codeArea.replaceWith('<div id="toolBar"></div><div id="editor"></div>');
+	$('#editor').css('position','relative');
+	$('#editor').css('width','100%');
+	$('#editor').css('height','500px');
+        
+	editor = ace.edit("editor");
+	editor.setValue(unescape(codeContent));
+	editor.setTheme("ace/theme/monokai");
+	editor.getSession().setMode(codeExtensionsDict[pageType]['aceMode']);
+        
+	mw.loader.using('mediawiki.api.edit', function() {
+	    $('#toolBar').html('<div class="codeEditOptions" style="position: relative;background-color: #F0F0F0;height: 30px;padding: 10px 10px 5px 10px;"><label style="float:left; line-height:24px">Editor theme : </label><select id="themeSelect" style="float: left; margin-left: 5px;margin-top: 3px;"></select><div class="editButtons" style="float: right;"><input id="wpCodeSave" type="submit" tabindex="5" value="Save" accesskey="s" title="Save your changes [alt-s]"><input id="wpCodeSaveDone" type="submit" tabindex="6" value="Close" accesskey="p" title="Close"></div><input class="mw-summary" id="wpSummary" maxlength="255" tabindex="1" size="60" spellcheck="true" title="Enter a short summary [alt-b]" accesskey="b" name="wpSummary" placeholder="Enter a brief summary of your changes" style="margin-top: 3px;margin-right: 5px;width: 50%;float: right;height: 17px;"><!-- editButtons --></div>');
 
-        $.getScript("//www.jshint.com/src/js/jshint.js",function(data,textStatus,jqxhr)
-                    {
-                        console.log("Loaded2");
-                    });
-    }
+	    for(var key in themeDict)
+	    {
+		$('#themeSelect').append('<option value="ace/theme/'+themeDict[key]+'">'+key+'</option>');
+	    }
+            
+	    $('#themeSelect').change(function(){
+		var themeString = $('#themeSelect').val();
+		editor.setTheme(themeString.toString());
+	    });
+            
+	    function saveCode() {
+		var edit = new mw.Api();
+		var params = {
+		    action: 'edit',
+		    title: mw.config.get('wgPageName'),
+		    text: editor.getSession().getValue(),
+		    summary: $('#wpSummmary').val()
+		};
+		edit.postWithEditToken(params).done(function(data){
+                    mw.util.jsMessage('Your code is saved');
+		});
+            }
+            
+	    $('#wpCodeSave').click(function(){
+                console.log("dadj");
+		saveCode();	
+	    });
+            
+	    $('#wpCodeSaveDone').click(function(){
+	        location.reload(true);
+            });
+         
+        });
+    });
+    
+    $.getScript("//d1n0x3qji82z53.cloudfront.net/src-min-noconflict/ace.js", function(data, textStatus, jqxhr) {
+	editCodeLink.css('pointer-events','auto');
+	editCodeLink.css('color','');	
+    });
+    
 }
-});
